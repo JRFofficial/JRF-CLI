013dc5cbf81dd517b4c86cf5234de524124adead
Uri Arev <uri.arev@gmail.com>
2020-12-17 19:14:40 +0200

    [patch] Adding About flag

--- main.c
+++ main.c
@@ -5,6 +5,8 @@ int main(int argc, char *argv[]) {
 	if ( argc >= 2 ) {
 		if ( strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "--debug") == 0 ) {
 			printf("DEBUG MODE");
+		} else if ( strcmp(argv[1], "-a") == 0 || strcmp(argv[1], "--about") == 0 ) {
+			printf("ABOUT");
 		};
 	} else {
 		//usage
