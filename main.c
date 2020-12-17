971921efdf84182b34514faa3a69e951ad8f4cbc
Uri Arev <uri.arev@gmail.com>
2020-12-17 12:16:32 +0200

    [patch] Adding --debug flag

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[]) {
 	if ( argc >= 2 ) {
-		if ( strcmp(argv[1], "-d") == 0 ) {
-			printf("DEBUG");
+		if ( strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "--debug") == 0 ) {
+			printf("DEBUG MODE");
 		};
 	} else {
 		//usage
